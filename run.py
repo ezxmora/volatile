2e930625c98387fc665b938d22b384966ef06a02
Eriknaitor <erik1997@hotmail.es>
2021-08-01 00:35:30 +0100

    Removed useless variables

--- run.py
+++ run.py
@@ -5,7 +5,8 @@ from utils.Volatile import Volatile
 from utils.config import auth
 
 bot = Volatile(auth)
-tweets = bot.deletetweets()
-retweets = bot.deleteretweets()
-likes = bot.deletelikes()
+bot.deletetweets()
+bot.deleteretweets()
+bot.deletelikes()
+
 bot.generatereport(True)
